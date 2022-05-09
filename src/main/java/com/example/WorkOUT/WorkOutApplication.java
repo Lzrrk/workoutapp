21fe9cb4168509c7217f7ace3075d0aa45c0fa45
Lazar Rakovic <lzrkofficial@gmail.com>
2022-05-09 11:46:42 +0200

    first commit

---
+++ src/main/java/com/example/WorkOUT/WorkOutApplication.java
@@ -0,0 +1,14 @@
+package com.example.WorkOUT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class WorkOutApplication extends SpringBootServletInitializer {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkOutApplication.class, args);
+	}
+
+}
