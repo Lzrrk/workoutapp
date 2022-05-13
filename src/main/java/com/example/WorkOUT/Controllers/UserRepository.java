18bc762ac2a6edec9b9ccb8a596c59a6dc329428
Johan Sandell <74353914+Johan-Sandell@users.noreply.github.com>
2022-05-13 11:41:36 +0200

    Added API coordinates and coversion of coordinates

--- src/main/java/com/example/WorkOUT/Controllers/UserRepository.java
+++ src/main/java/com/example/WorkOUT/Controllers/UserRepository.java
@@ -1,6 +1,12 @@
 package com.example.WorkOUT.Controllers;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
-public interface UserRepository extends CrudRepository<User,Integer> {
+@Repository
+public interface UserRepository extends JpaRepository<User,Integer> {
+
+    boolean existsByUserID(int userId);
+    boolean existsByEmail(String email);
 }
